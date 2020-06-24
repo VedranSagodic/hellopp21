7c7c51e6bada01648737a66f6aceb6bb02b86350
VedranSagodic <vedran.sagodic@gmail.com>
2020-06-24 11:26:37 +0200

    Prim broj raspon

--- zadaci/zadaca.php
+++ zadaci/zadaca.php
@@ -1,10 +1,27 @@
 <html>
 	<head>
-	<title>
-	 Raspon prim brojeva u php
-	 </title>
+	<meta charset="utf-8" />
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Ispis prim brojeva</title>
+<link rel="stylesheet" href="/assets/css/foundation.css">
  </head>
 <body>
+
+<div class="grid-container">
+<div class="grid-x grid-padding-x">
+<div class='large-12 cell'>
+<div class='callout'>
+
+<form action="">
+Manji broj
+<input type="number" name="b1" value="<?php echo $prviBroj; ?>">
+Veći broj
+<input type="number" name="b2" value="<?php echo $drugiBroj; ?>">
+
+<input class="button" type="submit" value="lista">
+
+</form>
  <?php
 $prviBroj = isset($_GET['prviBroj']) ? $_GET['prviBroj'] :0;
 $drugiBroj = isset($_GET['drugiBroj']) ? $_GET['drugiBroj'] :0;
